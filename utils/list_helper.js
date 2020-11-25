fd0e7fb874188f9690fcf0a39f20a43ce1e0b966
Raey-Abera <ab.raey@gmail.com>
2020-11-24 22:04:23 -0500

    implementing jest tests

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,7 +1,22 @@
-// const dummy = (blogs) => {
-//   // ...
-// }
+const dummy = (blogs) => {
+//   return 1
+  return blogs[0]
+}
 
-// module.exports = {
-//   dummy
-// }
+const totalLikes = (listWithOneBlog) => {
+  return listWithOneBlog[0].likes
+//   return 5
+}
+
+const favoriteBlog = (listWithBlogs) => {
+  const likes = listWithBlogs.map(blog => blog.likes)
+  const mostLikes = Math.max(...likes)
+  return listWithBlogs.find(blog => blog.likes === mostLikes)
+}
+
+module.exports = {
+  dummy,
+  totalLikes,
+  favoriteBlog
+}
+
